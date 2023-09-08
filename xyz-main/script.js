ba82f94d0b49e11aa69561879cc0159c16009b8d
pooja <poojadhaker1410@gmail.com>
2023-09-08 23:35:41 +0530

    MY FIRST

---
+++ xyz-main/script.js
@@ -0,0 +1,12 @@
+// 1 question simple interest 
+const  principal=Number(prompt("Enter principal amount"));
+const rate=Number(prompt("Enter rate of interest 1 to 20%"));
+const time=Number(prompt("for how many months enter 1-15"));
+alert((principal*rate*time)/100);
+// area of circle 
+const radius=Number(prompt("enter radius you will get the area"));
+alert((3.14*radius*radius));
+
+// volume of cube
+const edge=Number(prompt("enter edge you will get the area of cube"));
+alert((6*edge*edge));
